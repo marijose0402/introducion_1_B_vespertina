40500a2fcfa8aa5f7f8212f294b3f55b60178173
Alejandro <108289376+mau-guaman@users.noreply.github.com>
2022-08-16 15:25:01 -0500

    Conexión básica DataBase con PHP
    se agregan los archivos correspondientes a INTROSOFT para conectar un formulario con base de datos usando PHP

---
+++ introsoft/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Testing</title>
+</head>
+<body>
+<div>
+<div class="container">
+
+			<form action="proceso.php" method="post" class="was-validated">
+			  <div class="mb-3 mt-3">
+			    <label for="email" class="form-label">Email:</label>
+			    <input type="email" class="form-control" id="email" placeholder="Ingrese su correo electrónico" name="email" required>
+			  </div>
+			  <div class="mb-3">
+			    <label for="pwd" class="form-label">Contraseña:</label>
+			    <input type="password" class="form-control" id="pwd" placeholder="Ingrese su clave" name="pwd" maxlength="6" size="6" required>
+			  </div>
+			  <div class="form-check mb-3">
+			    
+			  </div>
+			  <button type="submit" class="btn btn-primary">Entrar</button>
+			</form>
+
+</div>
+<div>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</html>
